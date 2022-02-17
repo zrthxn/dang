f0b2bba2908a4fbd9367f0ed8ccf4c9f6b618669
Alisamar Husain <zrthxn@gmail.com>
2022-02-17 18:22:56 +0530

    Foundation of parsing

--- dang.c
+++ dang.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 typedef enum { false, true } bool;
+typedef unsigned int uint;
 typedef char* str;
+
+#define LF      '\n'
+#define CR      '\r'
+#define TAB	    '\t'
+#define SPACE   ' '
+#define COMMENT '#'
 
 void printall(str* array, size_t size) {
 	for (size_t i = 0; i < size; i++)
@@ -13,7 +21,7 @@ void printall(str* array, size_t size) {
 // --------------------------
 // Lexer --------------------
 
-void readTargetFile(str filename, str* buffer, unsigned int* size) {
+void readTargetFile(str filename, str* buffer, uint* size) {
 	/**
 	 * @brief Read file into buffer as string
 	 */
@@ -21,8 +29,7 @@ void readTargetFile(str filename, str* buffer, unsigned int* size) {
 	FILE* f_ptr = fopen(filename, "r");
 
 	if (f_ptr == NULL) {
-		fprintf(stderr, "\nCouldn't open file \"%s\"\n", filename);
-		fflush(stderr);
+		fprintf(stderr, "\nError: Couldn't open file \"%s\"\n", filename);
 		exit(1);
 	}
 
@@ -34,7 +41,7 @@ void readTargetFile(str filename, str* buffer, unsigned int* size) {
 	// Allocate that many bytes and read
 	*buffer = malloc((*size) * sizeof(char));
 
-	unsigned int index = 0;
+	uint index = 0;
 	while (index < *size) {
 		char c = fgetc(f_ptr);
 		if (c != EOF) 
@@ -49,13 +56,13 @@ void readTargetFile(str filename, str* buffer, unsigned int* size) {
 	fclose(f_ptr);
 }
 
-void lex(const str filename, str** lexicon, unsigned int* lexsize) {
+void lex(const str filename, str** lexicon, uint* lexsize) {
 	/**
 	 * @brief Lex file into logical words
 	 */
 
 	str buffer;
-	unsigned int size;
+	uint size;
 
 	readTargetFile(filename, &buffer, &size);
 	printf("\b\b\b, %d bytes.\n", size);
@@ -63,18 +70,22 @@ void lex(const str filename, str** lexicon, unsigned int* lexsize) {
 	str* __lexicon;
 	size_t length = 0;
 	str current = "";
-	unsigned int __ci = 0;
+	uint __ci = 0;
 
 	while (size) 
 	{
 		const char c = *buffer;
 
 		if (
-			// Ignore LF, TAB and SPACE, 
-			(c != '\n' && c != '\t' && c != ' ') ||
+			// Add new lex on LF, CR, TAB and SPACE, 
+			(c != LF && c != CR && c != TAB && c != SPACE) ||
+			
 			// unless first char of current word is quote
 			// and the quote isnt closed yet; for strings
-			(current[0] == '\"' && (__ci > 0 ? current[__ci-1] != '\"' : true))
+			(current[0] == '\"' && (__ci > 0 ? current[__ci-1] != '\"' : true)) ||
+
+			// Unless handling comments: ignore line until you see LF or CR
+			(current[0] == COMMENT && c != LF && c != CR)
 		)
 		{
 			char new[++__ci];
@@ -86,9 +97,20 @@ void lex(const str filename, str** lexicon, unsigned int* lexsize) {
 			current = malloc((__ci + 1) * sizeof(char));
 			strcpy(current, new);
 		}
-		// time to add a new lex
+		/**
+		 * @todo add conditions for condensed expressions
+		 * like `var bye:int = (23+45)*3/4`
+		 */
+		// Time to add a new lex
 		else if (strcmp(current, "") != 0)
 		{
+			if (current[0] == COMMENT) {
+				current = malloc(sizeof(char));
+				strcpy(current, "");
+				__ci = 0;
+				continue;
+			}
+
 			str __lexes[++length];
 			// Save previous entries
 			if (length > 1) for (size_t i = 0; i < length-1; i++) {
@@ -107,7 +129,6 @@ void lex(const str filename, str** lexicon, unsigned int* lexsize) {
 				strcpy(__lexicon[i], __lexes[i]);
 			}
 
-			// printf("LEX %s\n", __lexicon[length-1]);
 			current = malloc(sizeof(char));
 			strcpy(current, "");
 			__ci = 0;
@@ -119,7 +140,8 @@ void lex(const str filename, str** lexicon, unsigned int* lexsize) {
 
 	*lexsize = length;
 	*lexicon = malloc((*lexsize) * sizeof(str*));
-	for (size_t i = 0; i < *lexsize; i++) {
+	for (size_t i = 0; i < *lexsize; i++) 
+	{
 		(*lexicon)[i] = malloc(strlen(__lexicon[i]) * sizeof(char));
 		strcpy((*lexicon)[i], __lexicon[i]);
 	}
@@ -128,147 +150,562 @@ void lex(const str filename, str** lexicon, unsigned int* lexsize) {
 // --------------------------
 // Parser -------------------
 
-typedef enum
-{
-	VAR,
-	FN,
-	IF,
-	ELIF,
-	ELSE,
-	END,
-	WHILE,
-	DO,
-	INCLUDE,
-} Keyword;
+typedef 
+	enum { _ = 100,
+		VAR,
+		FN,
+		IF,
+		THEN,
+		ELIF,
+		ELSE,
+		END,
+		WHILE,
+		DO,
+		INCLUDE,
+		__KEYWORDS_COUNT,
+	} 
+Keyword;
 
-typedef enum
-{
-	// Binary Ops
-	ADD, // +
-	SUB, // -
-	MUL, // *
-	DIV, // /
-	MOD, // %
+typedef 
+	enum { __ = __KEYWORDS_COUNT,
+		// Binary Ops
+		ADD, // +
+		SUB, // -
+		MUL, // *
+		DIV, // /
+		MOD, // %
 
-	ASSIGN, // =
-	CALL,		// <|
+		ASSIGN, // =
+		CALL,		// <|
 
-	LOGICAL_GREATER_THAN, // >
-	LOGICAL_LESS_THAN,		// <
-	LOGICAL_EQUAL,				// ==
-	LOGICAL_NOT_EQUAL,		// !=
-	LOGICAL_AND,					// &&
-	LOGICAL_OR,						// ||
-	LOGICAL_XOR,					// ^^
+		LOGICAL_GREATER_THAN, // >
+		LOGICAL_LESS_THAN,		// <
+		LOGICAL_EQUAL,				// ==
+		LOGICAL_NOT_EQUAL,		// !=
+		LOGICAL_AND,					// &&
+		LOGICAL_OR,						// ||
+		LOGICAL_XOR,					// ^^
 
-	BIT_SHIFT_LEFT,	 // <<
-	BIT_SHIFT_RIGHT, // >>
+		BIT_SHIFT_LEFT,	 // <<
+		BIT_SHIFT_RIGHT, // >>
 
-	BIT_AND, // &
-	BIT_OR,	 // |
-	BIT_XOR, // ^
+		BIT_AND, // &
+		BIT_OR,	 // |
+		BIT_XOR, // ^
 
-	// Unary Ops
-	BIT_NOT,		 // !
-	LOGICAL_NOT, // !!
+		// Unary Ops
+		BIT_NOT,		 // !
+		LOGICAL_NOT, // !!
 
-	// INCREMENT, // ++
-	// DECREMENT, // --
-	// DEREF, // *
-} 
+		// INCREMENT, // ++
+		// DECREMENT, // --
+		// DEREF, // *
+		// ACCESSOR, // .
+		__OPERATIONS_COUNT,
+	} 
 Operator;
 
-typedef enum
-{
-	IntValue,
-	FloatValue,
-	StringValue,
-	NullValue,
-} ValueType;
+typedef 
+	enum { ___ = __OPERATIONS_COUNT,
+		IntValue,
+		FloatValue,
+		StringValue,
+		NullValue,
+		__VALUETYPES_COUNT,
+	} 
+ValueType;
 
-typedef struct
-{
-	ValueType type;
-	union
-	{
+typedef 
+	union {
 		int __i;
 		float __f;
 		str __s;
-	} value;
-} Literal;
+		void* n;
+	} 
+LiteralValue;
 
-typedef struct
-{
-	str name;
-	ValueType type;
-} Identifier;
+typedef 
+	struct {
+		ValueType type;
+		LiteralValue value;
+		uint msize; 
+	}
+Literal;
 
-typedef enum
-{
-	KeywordToken,
-	LiteralToken,
-	IdentifierToken,
-	OperatorToken,
-} TokenType;
+typedef 
+	struct {
+		str name;
+		ValueType type;
+		uint msize;
+	} 
+Identifier;
 
-typedef struct
-{
-	TokenType type;
-	union
-	{
+typedef 
+	struct {
+		str name;
+		ValueType type;
+		uint nargs;
+	} 
+Function;
+
+// Tokenization Types ----
+typedef 
+	enum { ____ = __VALUETYPES_COUNT,
+		KeywordToken,
+		LiteralToken,
+		DeclarationToken,
+		IdentifierToken,
+		OperatorToken,
+		ExpressionStartToken,
+		ExpressionEndToken,
+		__TOKENTYPES_COUNT
+	} 
+TokenType;
+
+typedef 
+	union {
 		Keyword __k;
 		Operator __o;
 		Identifier __i;
+		Function __f;
 		Literal __l;
-	} token;
-} Token;
+		void* n;
+	} 
+TokenValue;
 
+typedef 
+	struct {
+		TokenType type;
+		TokenValue value;
+	} 
+Token;
 
-Token* parse(const str filename, unsigned int* parselen) {
+typedef Token* TokenStream;
+
+void pushToken(TokenStream* stream, uint* len) {
+	TokenStream __new = malloc((++(*len)) * sizeof(Token));
+	if ((*len) > 1) for (size_t i = 0; i < (*len)-1; i++)
+		__new[i] = (*stream)[i];
+
+	// memcpy(&__new[(*len)-1], el, sizeof(Token));
+	__new[(*len)-1] = (Token){};
+	*stream = __new;
+}
+
+Token* popToken(TokenStream stream, uint* len) {
+	return &stream[--(*len)];
+}
+
+bool isInteger(str word) {
+	uint __i = 0;
+	if (word[0] == '-')
+		__i++;
+
+	while (word[__i] != '\0')
+		if (!isdigit(word[__i++]))
+			return false;
+
+	// str INT64_MAX_STR = "9223372036854775807";
+	// for (size_t i = 0; i < __i; i++) {
+	// 	if (INT64_MAX_STR[i] == '\0' || word[i] > INT64_MAX_STR[i]) {
+	// 		fprintf(stderr, "\nError: Integer \"%s\" larger than 64-bits.\n", word);
+	// 		exit(1);
+	// 	}
+	// }
+
+	return true;
+}
+
+bool isFloat(str word) {
+	uint __i = 0;
+	if (word[0] == '-')
+		__i++;
+
+	uint point = strcspn(word, ".");
+
+	/**
+	 * @todo Check floatable
+	 */
+
+	while (word[__i] != '\0')
+		if (!isdigit(word[__i]))
+			return false;
+
+	return true;
+}
+
+void parse(const str filename, TokenStream* stream, uint* parselen) 
+{
 	/**
 	 * @brief Parse words into token stream
 	 */
 
-	unsigned int lexsize = 0;
+	uint lexsize = 0;
 	str* lexicon; 
 	lex(filename, &lexicon, &lexsize);
 
-	printall(lexicon, lexsize);
-	exit(0);
-
-	Token* TokenStream = malloc(sizeof(Token));
-	unsigned int length = 0;
+	// Local token stream
+	TokenStream tstream;
+	// Length of token stream
+	uint length = 0;
 
 	while (lexsize)
 	{
 		const str word = *lexicon;
+		const uint len = strlen(word);
 
-		// *(&TokenStream[length]) = malloc(sizeof(Token));
+		if /* Include other files */ (strcmp(word, "include") == 0) {
+			lexicon = &lexicon[1];
+			lexsize--;
 
-		// Includes
-		if (strcmp(word, "include")) {
-			// parse( next word )
+			str arg = *lexicon;
+			// parse( next word as filepath )
 		}
+		else if /* Handle comments */ (word[0] == COMMENT) {
+			// Ignore them for now
+		}
+		else if /* Delimiter */ (strcmp(word, ";") == 0) {
+			// Ignore for now, auto splitting
+		}
+		else if /* Expression Start */ (
+			strcmp(word,  "[") == 0 || strcmp(word,  "{") == 0 || strcmp(word,  "(") == 0
+		) {
+			// check previous
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = ExpressionStartToken,
+				.value = (TokenValue)NULL,
+			};
+		}
+		else if /* Expression End */ (
+			strcmp(word,  "]") == 0 || strcmp(word,  "}") == 0 || strcmp(word,  ")") == 0
+		) {
+			// check previous
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = ExpressionEndToken,
+				.value = (TokenValue)NULL,
+			};
+		}
+		// Keywords ---------------------------------------------------------------
+		else if /* End */ (strcmp(word, "while") == 0) {
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = KeywordToken,
+				.value = (TokenValue)(Keyword)WHILE,
+			};
+		}
+		else if /* End */ (strcmp(word, "if") == 0) {
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = KeywordToken,
+				.value = (TokenValue)(Keyword)IF,
+			};
+		}
+		else if /* End */ (strcmp(word, "then") == 0) {
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = KeywordToken,
+				.value = (TokenValue)(Keyword)THEN,
+			};
+		}
+		else if /* End */ (strcmp(word, "elif") == 0) {
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = KeywordToken,
+				.value = (TokenValue)(Keyword)ELIF,
+			};
+		}
+		else if /* End */ (strcmp(word, "do") == 0) {
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = KeywordToken,
+				.value = (TokenValue)(Keyword)DO,
+			};
+		}
+		else if /* End */ (strcmp(word, "end") == 0) {
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = KeywordToken,
+				.value = (TokenValue)(Keyword)END,
+			};
+		}
+		// Declarations -----------------------------------------------------------
+		else if /* Variable declaration */ (strcmp(word, "var") == 0) {
+			lexicon = &lexicon[1];
+			lexsize--;
 
-		// Token new = {
-		// 	.type = KeywordToken,
-		// 	.token = END,
-		// 	.prev = TokenStream,
-		// 	.next = NULL,
-		// };
+			str arg = *lexicon;
+			uint split = strcspn(arg, ":");
+			if (split == strlen(arg)) { // Type of variable not given 
+				fprintf(stderr, "\nError: Untyped variable \"%s\" not supported yet.\n", arg);
+				exit(1);
+			}
 
-		// TokenStream->next = &new;
+			arg[split++] = '\0';
+			str type = &arg[split];
+
+			pushToken(&tstream, &length);
+
+			tstream[length-1] = (Token){
+				.type = DeclarationToken,
+				.value = (TokenValue)(Identifier){
+					.name = arg,
+					.msize = 0,
+					.type = 0,
+				}
+			};
+
+			strcpy(tstream[length-1].value.__i.name, arg);
+
+			if (strcmp(type, "int") == 0) {
+				tstream[length-1].value.__i.type = IntValue;
+				tstream[length-1].value.__i.msize = sizeof(__int64_t);
+			} else if (strcmp(type, "float") == 0) {
+				tstream[length-1].value.__i.type = FloatValue;
+				tstream[length-1].value.__i.msize = sizeof(float);
+			} else if (strcmp(type, "str") == 0) {
+				tstream[length-1].value.__i.type = StringValue;
+				tstream[length-1].value.__i.msize = sizeof(char*);
+			}
+		}
+		else if /* Function Declarations */ (strcmp(word, "fn") == 0) {
+			lexicon = &lexicon[1];
+			lexsize--;
+
+			str arg = *lexicon;
+			uint split = strcspn(arg, ":");
+			if (split == strlen(arg)) { // Type of function not given 
+				fprintf(stderr, "\nError: Untyped function \"%s\" not supported yet.\n", arg);
+				exit(1);
+			}
+
+			arg[split++] = '\0';
+			str type = &arg[split];
+
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = DeclarationToken,
+				.value = (TokenValue)(Function){
+					.name = arg,
+					.nargs = 0,
+					.type = 0,
+				}
+			};
+
+			strcpy(tstream[length-1].value.__i.name, arg);
+
+			if (strcmp(type, "int") == 0) {
+				tstream[length-1].value.__i.type = IntValue;
+				tstream[length-1].value.__i.msize = sizeof(__int64_t);
+			} else if (strcmp(type, "float") == 0) {
+				tstream[length-1].value.__i.type = FloatValue;
+				tstream[length-1].value.__i.msize = sizeof(float);
+			} else if (strcmp(type, "str") == 0) {
+				tstream[length-1].value.__i.type = StringValue;
+				tstream[length-1].value.__i.msize = sizeof(char*);
+			}
+
+			// add n args
+		}
+		// Operations -------------------------------------------------------------
+		else if /* Assignment Operation */ (strcmp(word, "=") == 0) {
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = OperatorToken,
+				.value = (TokenValue)(Operator)ASSIGN,
+			};
+		}
+		else if /* Addition Operation */ (strcmp(word, "+") == 0) {
+			Token* prev = popToken(tstream, &length);
+
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = OperatorToken,
+				.value = (TokenValue)(Operator)ADD,
+			};
+
+			pushToken(&tstream, &length);
+			memcpy(&tstream[length-1], prev, sizeof(Token));
+			// tstream[length-1] = *prev;
+		}
+		else if /* Subtraction Operation */ (strcmp(word, "-") == 0) {
+			Token* prev = popToken(tstream, &length);
+
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = OperatorToken,
+				.value = (TokenValue)(Operator)SUB,
+			};
+
+			pushToken(&tstream, &length);
+			memcpy(&tstream[length-1], prev, sizeof(Token));
+			// tstream[length-1] = *prev;
+		}
+		else if /* Multiplication Operation */ (strcmp(word, "*") == 0) {
+			Token* prev = popToken(tstream, &length);
+
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = OperatorToken,
+				.value = (TokenValue)(Operator)MUL,
+			};
+
+			pushToken(&tstream, &length);
+			memcpy(&tstream[length-1], prev, sizeof(Token));
+			// tstream[length-1] = *prev;
+		}
+		else if /* Division Operation */ (strcmp(word, "/") == 0) {
+			Token* prev = popToken(tstream, &length);
+
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = OperatorToken,
+				.value = (TokenValue)(Operator)DIV,
+			};
+
+			pushToken(&tstream, &length);
+			memcpy(&tstream[length-1], prev, sizeof(Token));
+			// tstream[length-1] = *prev;
+		}
+		else if /* Logical AND Operation */ (strcmp(word, "&&") == 0) {
+			Token* prev = popToken(tstream, &length);
+			// check if prev is a function identifier
+
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = OperatorToken,
+				.value = (TokenValue)(Operator)LOGICAL_AND,
+			};
+		}
+		else if /* Call Operation */ (strcmp(word, "<|") == 0) {
+			Token* prev = popToken(tstream, &length);
+
+			// check if prev is a function identifier
+			bool found = false;
+			for (size_t i = 0; i < length; i++) {
+				if (
+					tstream[i].type == DeclarationToken && 
+					strcmp(tstream[i].value.__f.name, prev->value.__f.name) == 0
+				) {
+					found = true;
+				}
+			}
+			
+			if (!found) {
+				fprintf(stderr, "\nError: Use of un-declared function \"%s\"\n", prev->value.__f.name);
+				exit(1);
+			}
+
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = OperatorToken,
+				.value = (TokenValue)(Operator)CALL,
+			};
+
+			pushToken(&tstream, &length);
+			memcpy(&tstream[length-1], prev, sizeof(Token));
+		}
+		// Literals ---------------------------------------------------------------
+		else if /* Int Literals */ (isInteger(word)) {
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = LiteralToken,
+				.value = (TokenValue)(Literal){
+					.type = IntValue,
+					.msize = sizeof(int),
+					.value = (LiteralValue)(int)atoi(word)
+				}
+			};
+		}
+		else if /* String Literals */ (word[0] == '\"' && word[len-1] == '\"' ) {
+			str value = &word[1];
+			value[len-2] = '\0';
+			
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = LiteralToken,
+				.value = (TokenValue)(Literal){
+					.type = StringValue,
+					.msize = (len-2) * sizeof(char),
+					.value = (LiteralValue)(str)value,
+				}
+			};
+			strcpy(tstream[length-1].value.__l.value.__s, value);
+		}
+		else if /* Float Literals */ (isFloat(word)) {
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = LiteralToken,
+				.value = (TokenValue)(Literal){
+					.type = FloatValue,
+					.msize = sizeof(float),
+					.value = (LiteralValue)(float)atof(word),
+				}
+			};
+		}
+		else if /* NULL Literals */ (strcmp(word, "null") == 0) {
+			pushToken(&tstream, &length);
+			tstream[length-1] = (Token){
+				.type = LiteralToken,
+				.value = (TokenValue)(Literal){
+					.type = NullValue,
+					.msize = sizeof(uint*),
+					.value = (LiteralValue)(int)0,
+				}
+			};
+		}
+		// Word or Identifier -----------------------------------------------------
+		else if (isalpha(word[0]) && strlen(word) <= 32) {
+			bool found = false;
+			for (size_t i = 0; i < length; i++) {
+				if (
+					tstream[i].type == DeclarationToken && 
+					strcmp(tstream[i].value.__i.name, word) == 0
+				) {
+					found = true;
+					pushToken(&tstream, &length);
+					tstream[length-1] = (Token){
+						.type = IdentifierToken,
+						.value = (TokenValue)tstream[i].value
+					};
+				}
+			}
+			
+			if (!found) {
+				fprintf(stderr, "\nError: Un-declared identifier \"%s\"\n", word);
+				exit(1);
+			}
+		} 
+		
+		else /* Something unrecognised was thrown own way */
+		{
+			fprintf(stderr, "\nError: Unknown word \"%s\"\n", word);
+			exit(1);
+		}
 
 		// Advance to next lex
 		lexicon = &lexicon[1];
 		lexsize--;
-
-		length++;
 	}
 
 	*parselen = length;
-	return TokenStream;
+	*stream = malloc((*parselen) * sizeof(Token));
+
+	if ((*parselen) > 1) for (size_t i = 0; i < (*parselen)-1; i++)
+		(*stream)[i] = tstream[i];
+
+	*stream = tstream;
 }
+
+// --------------------------
+// Targets ------------------
+
+void codegen_x86_64(Token* TokenStream, uint length, str buffer, int* buf_size) 
+{
+	
+}
+
 
 // --------------------------
 // Main ---------------------
@@ -299,12 +736,16 @@ void compileTarget(const str filename) {
 	 * @brief Generate code corresponding to stream 
 	 */
 
-	printf("Compiling target %s...", filename);
+	printf("[INFO] Compiling target %s...", filename);
 	fflush(stdout);
 
 	// lex and parse
-	unsigned int length = 0;
-	Token* stream = parse(filename, &length);
+	Token* stream;
+	uint length = 0;
+	parse(filename, &stream, &length);
+
+	printf("String: %s\n", stream[2].value.__l.value.__s);
+	return;
 
 	// Generate asm code
 	while (length)
@@ -337,7 +778,7 @@ int main(int argc, str argv[])
 		else if (isCompilerFlag(arg))
 			cflags[n_cflags++] = arg;
 		else
-			printf("Ignoring unknown argument \"%s\"\n", arg);
+			printf("[INFO] Ignoring unknown argument \"%s\"\n", arg);
 	}
 
 	for (size_t i = n_targets; i < __c; i++)
